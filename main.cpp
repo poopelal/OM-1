25a870ee64252b21f0591d4a5e1b22a70ade0310
poopelal <77093908+poopelal@users.noreply.github.com>
2021-02-17 19:01:24 +0200

    s
    trijsutis

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+
 using namespace std;
-int main() {
- 
-  int a;
-  cin >> a;
-  cout << "OM-" << a;
+
+int main(int argc, char **argv)
+{
+    int sideA, sideB, area;
+
+    cout << "garums: ";
+    cin >> sideA;
+    cout << "platums: ";
+    cin >> sideB;
+
+    area = sideA * sideB;
+
+    cout << "Taisnatura laukums : " << area << endl;
+    system("pause");
+    return 0;
 }
